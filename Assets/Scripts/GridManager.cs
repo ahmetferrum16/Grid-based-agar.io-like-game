17c1e0ac148b6d397c3be91781fe4b670cd7c210
ahmetferrum16 <ahmetefedemir0563@gmail.com>
2026-03-03 19:52:53 +0300

    unity update
    fixed the bugs from updating unity

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
-
 public class GridManager : MonoBehaviour
 {
-
     public int width, height;
     public GameObject tile;
-
     public GameObject lowHP;
     public GameObject highHP;
     private bool isSpawned = false;
     public int spawnFreq;
     public int targetCount;
     public int rndEnemySpawn;
+
+    public Transform playerTransform; // Player'ý inspector'dan baðla
 
     void Start()
     {
@@ -24,7 +23,6 @@ public class GridManager : MonoBehaviour
         rndEnemySpawn = Random.Range(1, 3);
     }
 
-    
     void Update()
     {
         if (GridMovement.MoveCount == targetCount && !isSpawned)
@@ -36,39 +34,50 @@ public class GridManager : MonoBehaviour
         }
         else
         {
-            isSpawned= false;
+            isSpawned = false;
         }
     }
-
     public void GenerateGrid()
     {
-        for (int i = 0; i < width ; i++){
-            for (int j = 0; j < height; j++){
-                var spawnedFile = Instantiate(tile, new Vector2(i, j),Quaternion.identity);
-
-
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                var spawnedFile = Instantiate(tile, new Vector2(i, j), Quaternion.identity);
             }
         }
-        
-    }
-    public static Vector3 randomPos()
-    {
-        int Xaxis = Random.Range(0, 15);
-        int Yaxis = Random.Range(0, 9);
 
-        return new Vector3(Xaxis, Yaxis, 0);
+    }
+
+    // Oyuncudan en az minDistance uzakta, grid içinde rastgele pozisyon üretir
+    public Vector3 randomPosAwayFromPlayer(float minDistance = 4f)
+    {
+        Vector3 spawnPos;
+        int attempts = 0;
+        do
+        {
+            int Xaxis = Random.Range(1, 14); // Kenarlardan 1 tile içeride
+            int Yaxis = Random.Range(1, 8);
+            spawnPos = new Vector3(Xaxis, Yaxis, 0);
+            attempts++;
+            if (attempts > 50) break; // Sonsuz döngü önlemi
+        }
+        while (playerTransform != null && Vector3.Distance(spawnPos, playerTransform.position) < minDistance);
+
+        return spawnPos;
     }
 
     public void spawnEnemy()
     {
+        Vector3 pos = randomPosAwayFromPlayer();
+
         if (rndEnemySpawn == 1)
-            Instantiate(lowHP, randomPos(), Quaternion.identity);
-            
+            Instantiate(lowHP, pos, Quaternion.identity);
+
         else if (rndEnemySpawn == 2)
         {
-            Instantiate(highHP, randomPos(), Quaternion.identity);
+            Instantiate(highHP, pos, Quaternion.identity);
         }
         rndEnemySpawn = Random.Range(1, 3);
     }
-
 }
